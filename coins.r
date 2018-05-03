0de9515ffe010a4e142b78497910683588df17fe
KBPsystem777 <koleen.bp@outlook.com>
2018-05-03 20:56:41 +0800

    Initial commit to github

--- coins.r
+++ coins.r
@@ -1,5 +1,5 @@
 #Install Packages
-install.packages(c("shiny", "tidyverse, shinydashboard, rvest"));
+install.packages(c("shiny", "tidyverse", "shinydashboard", "rvest"));
 
 #Load the installed Packages
 library(shiny)
@@ -7,45 +7,57 @@ library(tidyverse)
 library(shinydashboard)
 library(rvest)
 
-#Functions
-
-get.data <- function(x) {
+#Functions to get all data in the URL and store it on a dataFrame
+get.data <- function(x){
   
   #read the site as webpage
-  myurl <- read_html("https://coinmarketcap.com/gainers-losers")
+  myurl <- read_html("https://coinmarketcap.com/gainers-losers/")
   
   #convert html table to R Object
-  muyrl <- html_table(myurl)
+  myurl <- html_table(myurl)
+ 
   
   #get first item on the list
   to.parse <- myurl[[1]]
   
+  
   #data clean-up removing extra chars
-  to.parse$`% 1h` <- gsub("%", "", to.parse$`% 1h`)
+  to.parse$`% 1h` <- gsub("%","",to.parse$`% 1h`)
+ 
   
   #convert % column to numerals, so we can sort em out.
-  to.parse$`% 1h` <- as.numeric(to.parse$`% 1h`)
+  to.parse$`% 1h`<- as.numeric(to.parse$`% 1h`)
+  
   
   #convert Coin Symbol to factor
   to.parse$Symbol <- as.factor(to.parse$Symbol)
   
   #Sort by gain value and return the dataFrame
   to.parse$Symbol <- factor(to.parse$Symbol,
-                            levels = 
-                              to.parse$Symbol[order(to.parse$'% 1h')])
+                            levels = to.parse$Symbol[order(to.parse$'% 1h')])
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  #return finished dataFrame
+  to.parse
   
 }
+
+#Function to get highest value
+get.infobox.val <- function(x){
+  df1 <- get.data()
+  df1 <- df1$`% 1h`[1]
+  df1
+}
+
+#Function to get all top coin
+get.infobox.val <- function(x) {
+  df1 <- get.data()
+  df1 <- df1$`% 1h`[1]
+  df1
+}
+
+
+
+
+
+
+
